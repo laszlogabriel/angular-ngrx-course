d3abe59e54492d12e7557d9f8bfb4fdc27c1de2d
László Gábriel <laszlogabriel1@gmail.com>
2018-11-10 10:58:21 +0100

    lint fixes

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,12 +6,13 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import {User} from '../model/user.model';
-import {AuthActions, AuthActionTypes} from '../auth/auth.actions';
-import {storeFreeze} from 'ngrx-store-freeze';
-import {routerReducer} from '@ngrx/router-store';
+import { User } from '../model/user.model';
+import { AuthActions, AuthActionTypes } from '../auth/auth.actions';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { routerReducer } from '@ngrx/router-store';
 
 
+// tslint:disable-next-line:no-empty-interface
 export interface AppState {
 
 }
