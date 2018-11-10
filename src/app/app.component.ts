e3fc443c43510f374a49047ff1df554a20ae2748
László Gábriel <laszlogabriel1@gmail.com>
2018-11-10 10:55:04 +0100

    finish auth part of the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from './reducers';
+import { Logout } from './auth/auth.actions';
+import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +12,27 @@ import {Observable} from "rxjs";
 })
 export class AppComponent implements OnInit {
 
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
-    constructor() {
+  ngOnInit() {
+    this.isLoggedIn$ = this.store
+      .pipe(
+        select(isLoggedIn)
+      );
 
-    }
+    this.isLoggedOut$ = this.store
+      .pipe(
+        select(isLoggedOut)
+      );
 
-    ngOnInit() {
+  }
 
-
-    }
-
-    logout() {
-    }
-
-
+  logout() {
+    this.store.dispatch(new Logout());
+  }
 }
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,23 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../../environments/environment';
+import { routerReducer } from '@ngrx/router-store';
+
+// tslint:disable-next-line:no-empty-interface
+export interface AppState {
+
+}
+
+
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
+};
+
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
